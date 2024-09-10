bfbdb3c744798b936d6a3ec6d5dce3f2cfb1cc5a
Cru <lory.cruccu@gmail.com>
2024-09-10 15:04:27 +0200

    minor rename

--- start-database.sh
+++ start-database.sh
@@ -9,7 +9,7 @@
 
 # On Linux and macOS you can run this script directly - `./start-database.sh`
 
-DB_CONTAINER_NAME="hide-and-hit-postgres"
+DB_CONTAINER_NAME="my-love-memories-postgres"
 
 if ! [ -x "$(command -v docker)" ]; then
   echo -e "Docker is not installed. Please install docker and try again.\nDocker install guide: https://docs.docker.com/engine/install/"
@@ -50,6 +50,6 @@ docker run -d \
   --name $DB_CONTAINER_NAME \
   -e POSTGRES_USER="postgres" \
   -e POSTGRES_PASSWORD="$DB_PASSWORD" \
-  -e POSTGRES_DB=hide-and-hit \
+  -e POSTGRES_DB=my-love-memories \
   -p "$DB_PORT":5432 \
   docker.io/postgres && echo "Database container '$DB_CONTAINER_NAME' was successfully created"
